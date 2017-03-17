df9d6303192513eb1bccbb8cc6dc93f1fa527026
karendi <sharonkarendi16@gmail.com>
2017-03-17 12:33:52 +0300

    Configured the app to use url_for in tests

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 flask_app = Flask(__name__)
 flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + BASE_DIR + '/bucket_list.db'
 flask_app.config['JWT_SECRET_KEY'] = 'test123'
+flask_app.config['SERVER_NAME'] = '0.0.0.0'
 
 
 db = SQLAlchemy(flask_app)
@@ -17,7 +18,7 @@ from . import views
 
 api = Api(flask_app)
 api.add_resource(views.UserRegistration, "/v1/auth/register")
-api.add_resource(views.BucketList, "/v1/bucketlists")
+api.add_resource(views.BucketList, "/v1/bucketlists", endpoint='get_and_post_get_bucket_list')
 api.add_resource(views.SingleBucketList, "/v1/bucketlists/<int:id>")
 api.add_resource(views.Items, "/v1/bucketlists/<int:id>/items")
 api.add_resource(views.ItemsUpdate, "/v1/bucketlists/<int:id>/items/<int:item_id>")
