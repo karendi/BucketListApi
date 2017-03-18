a5c56a9a243603cecf8898f9caebda63806c4f99
karendi <sharonkarendi16@gmail.com>
2017-03-19 01:53:31 +0300

    [Feature] Added tests for the views

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 flask_app = Flask(__name__)
 flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + BASE_DIR + '/bucket_list.db'
 flask_app.config['JWT_SECRET_KEY'] = 'test123'
-flask_app.config['SERVER_NAME'] = '0.0.0.0'
 
 
 db = SQLAlchemy(flask_app)
@@ -19,7 +18,7 @@ from . import views
 api = Api(flask_app)
 api.add_resource(views.UserRegistration, "/v1/auth/register")
 api.add_resource(views.BucketList, "/v1/bucketlists", endpoint='get_and_post_get_bucket_list')
-api.add_resource(views.SingleBucketList, "/v1/bucketlists/<int:id>")
-api.add_resource(views.Items, "/v1/bucketlists/<int:id>/items")
-api.add_resource(views.ItemsUpdate, "/v1/bucketlists/<int:id>/items/<int:item_id>")
+api.add_resource(views.SingleBucketList, '/v1/bucketlists/<int:id>', endpoint='single_bucket_list')
+api.add_resource(views.Items, "/v1/bucketlists/<int:id>/items", endpoint='items')
+api.add_resource(views.ItemsUpdate, "/v1/bucketlists/<int:id>/items/<int:item_id>", endpoint='items_update')
 jwt = JWT(flask_app, views.authenticate, views.identity)
--- run_flask.py
+++ run_flask.py
@@ -1,4 +1,7 @@
 import app
 
 
-app.flask_app.run(debug=True)
+app.flask_app.run(host='0.0.0.0',
+                  debug=True,
+                  port=5000,
+                  )
